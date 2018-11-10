2d16b1318a0ee165f6d0ddf3ddcdf8c7ab910ee7
Matthew Garrett <mjg59@google.com>
2018-11-10 12:13:07 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 dynamic_requires = []
 
 setup(
-    name='antsar-avion',
-    version='0.9.1',
+    name='avion',
+    version='0.10',
     author='Matthew Garrett',
     author_email='mjg59@srcf.ucam.org',
     url='http://github.com/mjg59/python-avion',
